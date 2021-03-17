96402daa0ffc6eabeea3d626e7a1598319af42c0
brancomartinezc <bmartinezcarpineti@gmail.com>
2021-03-16 21:07:32 -0300

    changed search to json file and adapted the scraper

--- scraper.py
+++ scraper.py
@@ -1,19 +1,22 @@
 import re
+import json
 import requests
 from bs4 import BeautifulSoup
 
-""" From a txt file file creates a dictionary with techs to search """
-def create_techs_dict():
-    techs = {}
-    file = open('search.txt', 'r')
-    techs_list = file.read().split(" ")
 
+""" From the json file gets the position and creates a dictionary with techs to search"""
+def get_search_data():
+    with open('search.json') as file:
+        data = json.load(file)
+
+    position = data.get('position')
+    techs = {}
+    techs_list = data.get('techs')
     for tech in techs_list:
         techs.update({tech:0})
 
-    file.close()
-
-    return techs
+    #print(data.get('position'))
+    return position,techs
 
 
 """ Generates the url from position """
@@ -29,6 +32,7 @@ def get_job_description(post):
     job_url = 'https://www.indeed.com' + post.h2.a.get('href')
     response = requests.get(job_url)
     soup = BeautifulSoup(response.text, 'html.parser')
+
     try:
         job_desc = soup.find('div', 'jobsearch-jobDescriptionText').text
     except AttributeError:
@@ -38,7 +42,7 @@ def get_job_description(post):
     return job_desc
 
 
-""" count the techs that are in the job description """
+""" Counts the techs that are in the job description """
 def count_techs(description,techs):
     job_desc = re.split(r'[-,.\s]\s*',description)
     techs_to_add_one = []
@@ -47,15 +51,15 @@ def count_techs(description,techs):
         word_lower = word.lower()
         for tech in techs:
             if word_lower == tech:
-                if tech not in techs_to_add_one:
-                    techs_to_add_one.append(tech)
+                if tech not in techs_to_add_one: # it has to be in this way because in some jobs description 
+                    techs_to_add_one.append(tech) # the same technology/language is named more than once
 
     for tech in techs_to_add_one:
         techs[tech] += 1
 
 
-def main(position):
-    techs = create_techs_dict()
+def main():
+    position,techs = get_search_data()
     url = get_url(position)
 
     while True:
@@ -80,5 +84,6 @@ def main(position):
     
     print(f"final: {techs}") #debugger
 
+
 if __name__ == '__main__':
-    main('backend') #change the position to search
+    main() #change the position to search
